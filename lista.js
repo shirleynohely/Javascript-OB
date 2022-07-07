5bd3178d516f43d15b4452d13f8a22cf7eeeb7f1
Leyly <nohely.ramosderosas@gmail.com>
2022-07-07 17:11:16 -0300

    segunda entrega

---
+++ lista.js
@@ -0,0 +1,14 @@
+const lista = [
+  "Shirley",
+  34,
+  true,
+  new Date(1987,10,26),
+  libro = {
+    titulo: "El Alquimista",
+    autor: "Paulo Coelho",
+    fecha: new Date(1988, 2, 23),
+    url: "https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxob21vbGVjdG9yfGd4OjIyN2RiNjNmNDNlMDE2NzE",
+  }
+];
+
+console.log(lista);
